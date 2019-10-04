d2f23721ba3131f574b41021bc116eef3b493db5
safanbd <safanahmed5@gmail.com>
2019-10-04 15:12:14 +0600

    archive pages

--- template-parts/common/header/breadcamb.php
+++ template-parts/common/header/breadcamb.php
@@ -3,8 +3,27 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1><?php the_title(); ?></h1>
-                <h6><a href="<?php echo get_home_url(); ?>">Home</a> <i class="fas fa-angle-right"></i> <?php the_title(); ?></h6>
+                <h1>
+                	<?php
+                		if (is_archive()) {
+                			the_archive_title();
+                		}else{
+                			the_title();
+                		}
+                    	
+                	?>
+                </h1>
+                <h6>
+                	<a href="<?php echo get_home_url(); ?>">Home</a> <i class="fas fa-angle-right"></i> 
+                	<?php
+                		if (is_archive()) {
+                			the_archive_title();
+                		}else{
+                			the_title();
+                		}
+                    	
+                	?>
+                </h6>
             </div>
         </div>
     </div>
