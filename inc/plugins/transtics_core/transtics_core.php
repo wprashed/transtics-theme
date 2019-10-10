cc70de96fb643b8ab67cf2ffb69a352e8c235bbe
safanbd <safanahmed5@gmail.com>
2019-10-10 18:58:07 +0600

    core plugin included

---
+++ inc/plugins/transtics_core/transtics_core.php
@@ -0,0 +1,632 @@
+<?php
+
+/**
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Transtics Core
+ * Description:       Support plugin for Transtics theme.
+ * Version:           1.0.0
+ * Author:            Andit Themes
+ * Author URI:        https://anditthemes.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       transtics_core
+ * Domain Path:       /languages
+ */
+
+
+/* Custom Post */
+function cptui_register_my_cpts() {
+
+	/**
+	 * Post Type: Slider.
+	 */
+
+	$labels = array(
+		"name" => __( "Slider", "transtics" ),
+		"singular_name" => __( "Slider", "transtics" ),
+	);
+
+	$args = array(
+		"label" => __( "Slider", "transtics" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => true,
+		"rewrite" => array( "slug" => "slider", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-slides",
+		"supports" => array( "title", "editor" ),
+	);
+
+	register_post_type( "slider", $args );
+
+	/**
+	 * Post Type: Testimonial.
+	 */
+
+	$labels = array(
+		"name" => __( "Testimonial", "transtics" ),
+		"singular_name" => __( "Testimonial", "transtics" ),
+	);
+
+	$args = array(
+		"label" => __( "Testimonial", "transtics" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => true,
+		"rewrite" => array( "slug" => "testimonial", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-testimonial",
+		"supports" => array( "title", "editor", "thumbnail" ),
+	);
+
+	register_post_type( "testimonial", $args );
+
+	/**
+	 * Post Type: Service.
+	 */
+
+	$labels = array(
+		"name" => __( "Service", "transtics" ),
+		"singular_name" => __( "Service", "transtics" ),
+	);
+
+	$args = array(
+		"label" => __( "Service", "transtics" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => true,
+		"rewrite" => array( "slug" => "service", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-buddicons-topics",
+		"supports" => array( "title", "editor", "thumbnail" ),
+	);
+
+	register_post_type( "service", $args );
+
+	/**
+	 * Post Type: Team.
+	 */
+
+	$labels = array(
+		"name" => __( "Team", "transtics" ),
+		"singular_name" => __( "Team", "transtics" ),
+	);
+
+	$args = array(
+		"label" => __( "Team", "transtics" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => true,
+		"rewrite" => array( "slug" => "team", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-businessperson",
+		"supports" => array( "title", "thumbnail" ),
+	);
+
+	register_post_type( "team", $args );
+
+	/**
+	 * Post Type: Client.
+	 */
+
+	$labels = array(
+		"name" => __( "Client", "transtics" ),
+		"singular_name" => __( "Client", "transtics" ),
+	);
+
+	$args = array(
+		"label" => __( "Client", "transtics" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => true,
+		"rewrite" => array( "slug" => "client", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-money",
+		"supports" => array( "title", "thumbnail" ),
+	);
+
+	register_post_type( "client", $args );
+
+	/**
+	 * Post Type: Gallery.
+	 */
+
+	$labels = array(
+		"name" => __( "Gallery", "transtics" ),
+		"singular_name" => __( "Gallery", "transtics" ),
+	);
+
+	$args = array(
+		"label" => __( "Gallery", "transtics" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"delete_with_user" => false,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => true,
+		"rewrite" => array( "slug" => "gallery", "with_front" => true ),
+		"query_var" => true,
+		"menu_icon" => "dashicons-format-gallery",
+		"supports" => array( "title", "thumbnail" ),
+	);
+
+	register_post_type( "gallery", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts' );
+
+/* Custom Post Taxonomy */
+function cptui_register_my_taxes() {
+
+	/**
+	 * Taxonomy: Service Categoires.
+	 */
+
+	$labels = array(
+		"name" => __( "Service Categoires", "transtics" ),
+		"singular_name" => __( "Service Category", "transtics" ),
+	);
+
+	$args = array(
+		"label" => __( "Service Categoires", "transtics" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'service_category', 'with_front' => true,  'hierarchical' => true, ),
+		"show_admin_column" => true,
+		"show_in_rest" => true,
+		"rest_base" => "service_category",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => true,
+		);
+	register_taxonomy( "service_category", array( "service" ), $args );
+
+	/**
+	 * Taxonomy: Team Category.
+	 */
+
+	$labels = array(
+		"name" => __( "Team Category", "transtics" ),
+		"singular_name" => __( "Team Category", "transtics" ),
+	);
+
+	$args = array(
+		"label" => __( "Team Category", "transtics" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'team_category', 'with_front' => true,  'hierarchical' => true, ),
+		"show_admin_column" => true,
+		"show_in_rest" => true,
+		"rest_base" => "team_category",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		);
+	register_taxonomy( "team_category", array( "team" ), $args );
+
+	/**
+	 * Taxonomy: Gallery Category.
+	 */
+
+	$labels = array(
+		"name" => __( "Gallery Category", "transtics" ),
+		"singular_name" => __( "Gallery Category", "transtics" ),
+	);
+
+	$args = array(
+		"label" => __( "Gallery Category", "transtics" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => array( 'slug' => 'gallery_category', 'with_front' => true,  'hierarchical' => true, ),
+		"show_admin_column" => true,
+		"show_in_rest" => true,
+		"rest_base" => "gallery_category",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => true,
+		);
+	register_taxonomy( "gallery_category", array( "gallery" ), $args );
+}
+add_action( 'init', 'cptui_register_my_taxes' );
+
+/* Custom Fileds */
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_5d9b0bce5e919',
+	'title' => 'Slider',
+	'fields' => array(
+		array(
+			'key' => 'field_5d9b0bd757f12',
+			'label' => 'Button Text',
+			'name' => 'button_text',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5d9b0beb57f13',
+			'label' => 'Button Url',
+			'name' => 'button_url',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'slider',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+acf_add_local_field_group(array(
+	'key' => 'group_5d9aed4dd287e',
+	'title' => 'Team',
+	'fields' => array(
+		array(
+			'key' => 'field_5d9aed5734eb2',
+			'label' => 'Designation',
+			'name' => 'designation',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5d9aed6834eb3',
+			'label' => 'Facebook',
+			'name' => 'facebook',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => 'https://facebook.com/safan0',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5d9aed8f34eb4',
+			'label' => 'Twitter',
+			'name' => 'twitter',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => 'https://twitter.com/bitgatex',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5d9aedc034eb5',
+			'label' => 'Linkedin',
+			'name' => 'linkedin',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => 'https://www.linkedin.com/in/williamhgates',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'team',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+acf_add_local_field_group(array(
+	'key' => 'group_5d9ad8f3e23c7',
+	'title' => 'Testimonial',
+	'fields' => array(
+		array(
+			'key' => 'field_5d9ad90347c20',
+			'label' => 'Designation',
+			'name' => 'designation',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'testimonial',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+acf_add_local_field_group(array(
+	'key' => 'group_5d9d9c8f62b76',
+	'title' => 'User Social',
+	'fields' => array(
+		array(
+			'key' => 'field_5d9d9cf677da4',
+			'label' => 'Facebook',
+			'name' => 'facebook',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5d9d9cff77da5',
+			'label' => 'Twitter',
+			'name' => 'twitter',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5d9d9d0777da6',
+			'label' => 'Linkedin',
+			'name' => 'linkedin',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5d9d9d0e77da7',
+			'label' => 'Medium',
+			'name' => 'medium',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'user_role',
+				'operator' => '==',
+				'value' => 'all',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+endif;
+
