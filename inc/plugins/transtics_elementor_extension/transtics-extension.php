710d058bbe8309da3009cd281c30eb83dacf28d6
safanbd <safanahmed5@gmail.com>
2019-10-20 16:16:11 +0600

    gallery widget updated

--- inc/plugins/transtics_elementor_extension/transtics-extension.php
+++ inc/plugins/transtics_elementor_extension/transtics-extension.php
@@ -139,6 +139,7 @@ final class Transtics_Elementor_Extension {
 		require_once( __DIR__ . '/widgets/intro.php' );
 		require_once( __DIR__ . '/widgets/advantages.php' );
 		require_once( __DIR__ . '/widgets/contact.php' );
+		require_once( __DIR__ . '/widgets/gallery.php' );
 
 		// Register widget
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Transtics_Slider_widget() );
@@ -150,6 +151,7 @@ final class Transtics_Elementor_Extension {
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Transtics_Intro_widget() );
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Transtics_Advantages_widget() );
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Transtics_Contact_widget() );
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Transtics_Gallery_widget() );
 
 	}
 
